0a2a72c7af6b5dfc3e23eedc2ea557cdde5e6759
Christian Iversen <ci@iversenit.dk>
2016-09-27 18:12:40 +0200

    * Updated regexes in tricorder, implemented new protocol tagging options

--- tricorder.py
+++ tricorder.py
@@ -4,9 +4,19 @@ import re
 import sys, os
 import fileinput
 
-RE_COMMENT  = re.compile("#.*")
-RE_METADATA = re.compile("!(\w+)=(.+)(oh \s*#.*)?")
-RE_PACKET   = re.compile("([CS]\d*)\s+([A-Fa-f0-9]+)(?:\s+@([0-9]+))?(\s*#.*)?")
+RE_BITS = {
+    "packet": "([CS]\d*)\s+([A-Fa-f0-9]+)",
+    "metadata": "!(?P<key>\w+)(?:=(?P<value>\S+?))?",
+    "comment": "(?:#\s*(?P<comment>.*))?",
+    "timestamp": "(?:@(?P<timestamp>[0-9]+))?",
+}
+RE_COMMENT  = re.compile("^%(comment)s$" % RE_BITS)
+RE_METADATA = re.compile("^%(metadata)s\s*%(timestamp)s\s*%(comment)s$" % RE_BITS)
+RE_PACKET   = re.compile("^%(packet)s\s*%(timestamp)s\s*%(comment)s$" % RE_BITS)
+
+if len(sys.argv) < 2:
+    print("Usage: %s <apd files..>" % sys.argv[0])
+    sys.exit(1)
 
 for index, line in enumerate(fileinput.input(files=sys.argv[1:])):
     # remove leading and trailing whitespace
@@ -19,7 +29,7 @@ for index, line in enumerate(fileinput.input(files=sys.argv[1:])):
     # check for metadata
     metadata = RE_METADATA.match(line)
     if metadata:
-        print("Metadata: %r -> %r" % (metadata.group(1), metadata.group(2)))
+        print("Metadata: %r -> %r" % (metadata.group("key"), metadata.group("value")))
         continue
 
     # check for packets
